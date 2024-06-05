f13d4b46954a75ecfd1d107fecc8ecbfad5d4174
krypton-byte <rosid6434@gmail.com>
2024-06-05 21:30:21 +0700

    ci/cd:

--- docs/build.py
+++ docs/build.py
@@ -2,5 +2,5 @@ from subprocess import call
 from pathlib import Path
 import shlex
 workdir = Path(__file__).parent.parent
-call(shlex.split("sphinx-apidoc -o docs/source thundra thundra.core thundra.profiler thundra.storage"))
+call(shlex.split("poetry run sphinx-apidoc -o docs/source thundra thundra.core thundra.profiler thundra.storage"))
 call(shlex.split("make html"), cwd=workdir / "docs")
