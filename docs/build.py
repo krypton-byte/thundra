c7dc21125edd07d4df0c359c91d337ef28f935e0
krypton-byte <rosid6434@gmail.com>
2024-06-05 22:19:42 +0700

    .nojekyll

--- docs/build.py
+++ docs/build.py
@@ -4,3 +4,5 @@ import shlex
 workdir = Path(__file__).parent.parent
 call(shlex.split("poetry run sphinx-apidoc -o docs/source thundra thundra.core thundra.profiler thundra.storage"))
 call(shlex.split("poetry run make html"), cwd=workdir / "docs")
+with open(workdir / "_build/html/.nojekyll", "wb") as file:
+    file.write(b"")
--- docs/conf.py
+++ docs/conf.py
@@ -41,4 +41,4 @@ autodoc_default_flags = ["members"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-# html_static_path = ['_static']
+html_static_path = ['static']
